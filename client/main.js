80940e431d4ebb73e5b4b32f2adf07eea47b63d6
GitInMaRepo <oliver.s.roth@gmail.com>
2018-05-14 10:00:06 +0200

    second test: landing page

--- client/main.js
+++ client/main.js
@@ -1,6 +1,15 @@
 var example = {};
 
+example.showOption = function() {
+    return $('<div class="option-page">')
+}
+
 example.showView = function(hash) {
-    var value = $('<div class="option-page">');
-    return $('.view-container').empty().append(value);
+    var routes = {
+        '#option-1' : example.showOption
+    }
+    var value = routes[hash];
+    if(value) {
+        return $('.view-container').empty().append(value());
+    }
 }
--- client/tests/main_spec.js
+++ client/tests/main_spec.js
@@ -1,6 +1,11 @@
 describe('The router', function () {
     it('shows the options page', function() {
-        example.showView('#option');
+        example.showView('#option-1');
         expect($('.view-container .option-page').length).toEqual(1);
+    });
+
+    it('shows the landing page when no hash is given', function() {
+        example.showView('');
+        expect($('.view-container .landing-page').length).toEqual(1);
     });
 });
